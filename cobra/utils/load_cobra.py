a3d83e37747074a7bb58d1790038abb295fbd4e5
s1787956 <tim.lenz@tu-dresden.de>
2024-12-13 14:44:13 +0000

    ready for transfer

--- cobra/utils/load_cobra.py
+++ cobra/utils/load_cobra.py
@@ -3,11 +3,14 @@ from huggingface_hub import login, hf_hub_download
 from cobra.model.cobra import Cobra
 import torch
 import warnings
+import os
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-def get_cobra(download_weights=False,checkpoint_path=None):
+def get_cobra(download_weights=False,checkpoint_path=None,local_dir="weights"):
     if download_weights:
-        checkpoint_path = hf_hub_download("TLenz/COBRA", filename="pytorch_model.bin", local_dir="weights/", force_download=True)
+        if not os.path.exists(local_dir):
+            os.makedirs(local_dir)
+        checkpoint_path = hf_hub_download("TLenz/COBRA", filename="pytorch_model.bin", local_dir="weights", force_download=True)
         print(f"Saving model to {checkpoint_path}")
     state_dict = torch.load(checkpoint_path, map_location="cpu")
     model = Cobra()

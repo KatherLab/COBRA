13b58cede17f680a7e316972b06615d2fa356283
s1787956 <tim.lenz@tu-dresden.de>
2024-12-13 17:30:12 +0000

    added permission error handling for model download

--- cobra/utils/load_cobra.py
+++ cobra/utils/load_cobra.py
@@ -10,7 +10,13 @@ def get_cobra(download_weights=False,checkpoint_path=None,local_dir="weights"):
     if download_weights:
         if not os.path.exists(local_dir):
             os.makedirs(local_dir)
-        checkpoint_path = hf_hub_download("KatherLab/COBRA", filename="pytorch_model.bin", local_dir="weights", force_download=True)
+        try:
+            checkpoint_path = hf_hub_download("KatherLab/COBRA", filename="pytorch_model.bin", local_dir="weights", force_download=True)
+        except Exception as e:
+            if "403 Client Error" in str(e):
+                raise PermissionError("You do not have permission to access this model. Please ensure you have accepted the model's terms and conditions on https://huggingface.co/KatherLab/COBRA.")
+            else:
+                raise e
         print(f"Saving model to {checkpoint_path}")
     state_dict = torch.load(checkpoint_path, map_location="cpu")
     model = Cobra()
